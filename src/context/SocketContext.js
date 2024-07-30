5b56ef1ff46cbfbf94f4d58ce763c3ef2c3b7497
daviddexteel <dsilvestri@dexteel.com>
2024-07-30 11:22:39 -0300

    index and SocketContext changed

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -8,19 +8,19 @@ export const SocketContext = createContext();
 export const SocketProvider = ({ children }) => {
 
     const { socket, online,connectSocket,disconnectSocket } = useSocket('http://localhost:8080');
-    // const { auth } = useContext(AuthContext);
+     const { auth } = useContext(AuthContext);
     
-    // useEffect(() => {
-    //   if (auth.logged){
-    //     connectSocket();
-    //   }
-    // }, [auth,connectSocket]);
+    useEffect(() => {
+      if (auth.logged){
+        connectSocket();
+      }
+    }, [auth,connectSocket]);
     
-    // useEffect(() => {
-    //     if (!auth.logged){
-    //         disconnectSocket();
-    //     }
-    //   }, [auth,disconnectSocket]);
+    useEffect(() => {
+        if (!auth.logged){
+            disconnectSocket();
+        }
+      }, [auth,disconnectSocket]);
 
     return (
         <SocketContext.Provider value={{ socket, online }}>
--- src/index.js
+++ src/index.js
@@ -5,12 +5,8 @@ import { DevicesApp } from './DevicesApp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { SocketProvider } from './context/SocketContext';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <SocketProvider>
 
         <DevicesApp />
-    </SocketProvider>
 );
