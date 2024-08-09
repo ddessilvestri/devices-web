157352f083ca83f5cf88553a7d5d25bac0fe55ec
daviddexteel <dsilvestri@dexteel.com>
2024-08-09 11:26:18 -0300

    index.js: minor change

--- src/index.js
+++ src/index.js
@@ -6,7 +6,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-        <DevicesApp />
-);
+root.render(<DevicesApp />);
